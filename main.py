d641bc656ad58e1d27cb3ac494db9b855a3f7f1f
RudareanuLeonard <93031391+RudareanuLeonard@users.noreply.github.com>
2025-01-15 12:08:30 +0200

    kinda get the leagues names, but have some errors

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+import re
 
 SITE = "https://www.oddsportal.com/"
 SITE_FOOTBALL = "https://www.oddsportal.com/football/"
 
 
-def get_football_data():
+def get_football_countries():
     driver = webdriver.Chrome() #chrome webdriver
     driver.get(SITE_FOOTBALL)
+    # print(f"DRIVER = {type(driver)}")
+    # DRIVER = <class 'selenium.webdriver.chrome.webdriver.WebDriver'>
     # driver.fullscreen_window()
     driver.execute_script("document.body.style.zoom='10%'")
 
@@ -19,29 +22,81 @@ def get_football_data():
         get_countries = driver.find_elements(By.XPATH,'//div/div/a[contains(@href,"football/")]')
     except Exception as e:
         print(f"Exception while finding")
-    
-    #now i need to format the list with countries to make it look appropiate for accessing as website
-    # countries_formatted = []
 
-    # for el in get_countries:
-    #     country = el.text.lower()
-    #     country = country.replace(' ','-')
-    #     print(f"country = {country}")
+    return driver,get_countries
 
+
+def transform_country_to_url(country):
+    country = country.lower()
+    country = country.replace(" ", "-")
+
+    return country
+
+
+def transform_league_to_text(league):
+    str = str.replace("(", "")
+    str = str.replace(")", "")
+    str = re.sub("[0-9]", "", str)
+
+
+def get_football_data():
+
+    call_get_football_countries = get_football_countries()
+
+    country_leagues = {}
     
-    for country in get_countries: #for it to can be clicked it needs to be "visible" - so my solution was to zoom out the page
-        
-        # print(f"country = {country.text}")
+
+    driver = call_get_football_countries[0]
+    get_countries = call_get_football_countries[1] #i need to use he ".text"
+
+    # print(f"driver = {driver}")
+
+    country_to_url = []
+
+    for country in get_countries: #modify country string so i can add it to the xpath string
+        country_to_url.append(transform_country_to_url(country.text))
+    
+    # print(f"len get_countries = {len(get_countries)}")
+    # print(f"len country_to_url = {len(country_to_url)}")
+
+    for i in range (0, len(get_countries)): #for it to can be clicked it needs to be "visible" - so my solution was to zoom out the page
+        country = get_countries[i] #webelement
+        country_url = country_to_url[i]
 
         try:
-            
             country.click()
-            time.sleep(10)
+            time.sleep(5)
+
+            xpath = '//li/a[contains(@href, "football/' + country_url + '/")]'
+
+            # print(f"xpath = {xpath}")
+
+
+            find_leagues = driver.find_elements(By.XPATH, xpath)
+
+            for el in find_leagues:
+                print(el.text)
+
+            
+            # for el in find_leagues:
+            #     leagues = []
+            #     leagues.append(transform_league_to_text(el.text))
+
+            
+            # country_leagues[country] = leagues
+                
+
+            time.sleep(3)
+
+            # print(f"country leagues = {country_leagues[country]}")
+
             driver.back()
         except Exception as e:
-            print(f"exception occured")
-            time.sleep(10)
-            print()
+            print(f"error = {e}")
+
+
+
+
     driver.quit()
 
 if __name__ == "__main__":
